5a0d09ea4774851e24f58ad843e63b28ce4669de
chuck <charlesvincentanderson@gmail.com>
2021-06-01 17:35:20 -0400

    trying email in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,14 +61,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "radiate_consulting_skill_test_production"
 
   config.action_mailer.perform_caching = false
-#  ActionMailer::Base.smtp_settings = {
-#    address: "smtp.sendgrid.net",
-#    port: 587,
-#    domain: "radiate-consulting-skill-test.herokuapp.com",
-#    authentication: :plain,
-#    user_name: ENV['SENDGRID_USERNAME'],
-#    password: ENV['SENDGRID_PASSWORD']
-#  }
+  config.action_mailer.default_url_options = { :host => 'radiate-consulting-skill-test.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address          => "smtp.sendgrid.net",
+    :port             => "25",
+    :authentication   => :plain,
+    :user_name        => ENV['SENDGRID_USERNAME'],
+    :password         => ENV['SENDGRID_PASSWORD']
+#    :domain           => ENV['SENDGRID_DOMAIN']
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
